b7a18e9cd348a98a72c74d2e4241de340f16a6b8
Aakash <Aakash.21BT@dpgitm.ac.in>
2023-07-08 11:19:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express=require("express");
+const got = require("got"); //npm install got@9.6.0
+const bodyParser=require("body-parser");
+const app=express();
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+mailchimp.setConfig({
+  apiKey: "9bf478c3c7936d9b66085860d8d5c28e-us17",
+  server: "us17",
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+});
+app.post('/',async function(req,res){
+    const fname=req.body.Fname;
+    const lname=req.body.Lname;
+    const email=req.body.Email;
+    const listId="2f6105ab43";
+   
+    try{
+        //tocheck api connection
+        // const response = await mailchimp.ping.get();
+        //  console.log(response);
+
+
+        
+        const response = await mailchimp.lists.addListMember(listId, {
+            email_address:email,
+            status: "subscribed",
+            merge_fields: {
+              FNAME: fname,
+              LNAME: lname
+            }
+          });
+          console.log(response);
+          res.sendFile(__dirname+"/success.html");
+
+    }
+    catch(err){
+        console.log(err);
+        res.sendFile(__dirname+"/failure.html");
+    }
+}
+);
+
+app.post('/failure',function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
+
+app.listen(3000,()=> console.log("listening on port 3000"));
